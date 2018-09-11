ce4683a0fffe0e0932f91045fab2e080b4d9e03a
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-09-10 21:46:46 -0600

    recoded some aspects of users controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(request.env["omniauth.auth"])
+    auth = request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth['provider'], auth['uid']) || User.from_omniauth(auth)
     session[:user_id] = user.id
     redirect_to dashboard_path
   end
