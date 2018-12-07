0effd2bfe987b6381ec5873a81308ca3a0bc2c44
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-12-07 12:20:45 -0700

    temporarily disables vcr/webmock to further test other aspects of app

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,12 +13,12 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'vcr'
-VCR.configure do |c|
-  c.cassette_library_dir = 'spec/vcr/fixtures'
-  c.ignore_localhost = true
-  c.hook_into :webmock
-  # c.configure_rspec_metadata!
-end
+# VCR.configure do |c|
+#   c.cassette_library_dir = 'spec/vcr/fixtures'
+#   c.ignore_localhost = true
+#   c.hook_into :webmock
+#   # c.configure_rspec_metadata!
+# end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -75,10 +75,10 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.before(:each) do
-    WebMock.reset!
-    WebMock.disable_net_connect!
-  end
+  # config.before(:each) do
+  #   WebMock.reset!
+  #   WebMock.disable_net_connect!
+  # end
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
